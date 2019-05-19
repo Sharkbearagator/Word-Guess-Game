c85951d15546de49e07266a20779582bf856533e
Tyler Nichols <tegulator98@gmail.com>
2019-05-18 18:53:16 -0600

    More fixes of HTML and java var created.

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var wins = [1,2,3,4,5,6,7,8,9,10]
+var guessRemains = [1,2,3,4,5,6,7,8,9,10,11,12]
+var guessLetters = ['abcdefghijklmnopqrstuvwxyz'].split('')
+
+
+
+
+
+
+document.onkeyup = function(event){
+
+}
