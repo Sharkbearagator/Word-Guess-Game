e3106e2cd5d356a6a7db658f2759eb4bbd372625
Tyler Nichols <tegulator98@gmail.com>
2019-05-20 20:52:41 -0600

    Added all images, added a better array added divs.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
-var wins = [1,2,3,4,5,6,7,8,9,10]
+var wins = ["Wins: 1","Wins: 2","Wins: 3","Wins: 4","Wins: 5","Wins: 6","Wins: 7","Wins: 8","Wins: 9","Wins: 10"]
 var guessRemains = [1,2,3,4,5,6,7,8,9,10,11,12]
 var guessLetters = ['abcdefghijklmnopqrstuvwxyz'].split('')
+var bandNames = ["green-day", "linkin-park","gorillaz","eminem","fall-out-boy"]
 
 
 
