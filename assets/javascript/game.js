8560e1db6c09d19ec29e33467bb06206873864c2
Tyler Nichols <tegulator98@gmail.com>
2019-05-22 21:24:09 -0600

    Worked on loop not much done.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,14 @@
 var wins = ["Wins: 1","Wins: 2","Wins: 3","Wins: 4","Wins: 5","Wins: 6","Wins: 7","Wins: 8","Wins: 9","Wins: 10"]
-var guessRemains = [1,2,3,4,5,6,7,8,9,10,11,12]
-var guessLetters = ['abcdefghijklmnopqrstuvwxyz'].split('')
+var guessRemains = ["Guesses Remaining: 1","Guesses Remaining: 2","Guesses Remaining: 3","Guesses Remaining: 4","Guesses Remaining: 5" ,"Guesses Remaining: 6","Guesses Remaining: 7","Guesses Remaining: 8","Guesses Remaining: 9","Guesses Remaining: 10","Guesses Remaining: 11","Guesses Remaining: 12"]
+var guessLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var bandNames = ["green-day", "linkin-park","gorillaz","eminem","fall-out-boy"]
+$(document).on(function() {
 
 
-
-
-
-
-document.onkeyup = function(event){
-    for(var i = 12; i > guessRemains.length; i--){
-$("#guessRemains").html(guessRemains)
+    $("#guesses").on(KeyboardEvent);{
+        for(var i = 11; i <= guessRemains.length; i--){
+        $("#guesses").html(guessRemains[i]);    
+            }
     }
 }
+    )
