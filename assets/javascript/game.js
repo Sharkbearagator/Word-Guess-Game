cd8753e19a57d8a570268e4258741eaa42d4d0a4
Tyler Nichols <tegulator98@gmail.com>
2019-05-27 23:55:26 -0600

    fixed bugs and made a more efficeint letter check

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,23 @@
-var wins = ["Wins: 1","Wins: 2","Wins: 3","Wins: 4","Wins: 5","Wins: 6","Wins: 7","Wins: 8","Wins: 9","Wins: 10"]
-var guessRemains = ["Guesses Remaining: 1","Guesses Remaining: 2","Guesses Remaining: 3","Guesses Remaining: 4","Guesses Remaining: 5" ,"Guesses Remaining: 6","Guesses Remaining: 7","Guesses Remaining: 8","Guesses Remaining: 9","Guesses Remaining: 10","Guesses Remaining: 11","Guesses Remaining: 12"]
+var wins = 0;
+var guessRemains = 12
 var guessLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var bandNames = ["green-day", "linkin-park","gorillaz","eminem","fall-out-boy"]
-$(document).on(function() {
+var bandNames = ["Green Day", "Linkin Park","Gorillaz","Eminem","Fall Out Boy"]
+var random = bandNames[Math.floor(Math.random()*bandNames.length)];
 
+$(random).html("#answer");
 
-    $("#guesses").on(KeyboardEvent);{
-        for(var i = 11; i <= guessRemains.length; i--){
-        $("#guesses").html(guessRemains[i]);    
-            }
+var html = ""
+for(var j = 0; j < bandNames; j++){
+    html +="<span data-letter=" + word.charAt(j) + "";
+}
+
+document.onkeyup = function(e){
+    var letter = e.key.toLowerCase();
+    for(var i = 0; i < bandNames.length; i++){
+        if(letter === random.charAt(i)){
+            var domLetter = document.getElementById(letter);
+            domLetter.innerHTML = random.charAt(i);
+        }
     }
 }
-    )
+console.log(random);
