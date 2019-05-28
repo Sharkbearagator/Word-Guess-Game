2d44ea86b3fd81be0b9cb80d4a7a9ecc0bc2189d
Tyler Nichols <tegulator98@gmail.com>
2019-05-28 00:43:24 -0600

    adding the answer array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,14 @@
 var wins = 0;
 var guessRemains = 12
 var guessLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var bandNames = ["Green Day", "Linkin Park","Gorillaz","Eminem","Fall Out Boy"]
+var bandNames = ["Green Day" ,"Linkin Park","Gorillaz","Eminem","Fall Out Boy"];
 var random = bandNames[Math.floor(Math.random()*bandNames.length)];
+var answerArray = [];
 
-$(random).html("#answer");
-
-var html = ""
-for(var j = 0; j < bandNames; j++){
-    html +="<span data-letter=" + word.charAt(j) + "";
-}
+for (var i = 0; i < random.length; i++) {
+    answerArray[i] = "_";
+   }
+   var remainingLetters = random.length;
 
 document.onkeyup = function(e){
     var letter = e.key.toLowerCase();
@@ -17,7 +16,11 @@ document.onkeyup = function(e){
         if(letter === random.charAt(i)){
             var domLetter = document.getElementById(letter);
             domLetter.innerHTML = random.charAt(i);
+            document.getElementById("#word").html(domLetter);
         }
     }
+}
+if(document.getElementById("#word") === random){
+    wins++
 }
 console.log(random);
