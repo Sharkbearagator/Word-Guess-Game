81d2928ea8d0fb0d81d10a38ba74fa210019a023
Tyler Nichols <tegulator98@gmail.com>
2019-05-29 16:36:26 -0600

    added psuedo code and fixed song names to bands.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//set var and arrays
 var wins = 0;
 var guessRemains = 12
 var guessLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
@@ -5,13 +6,15 @@ var bandNames = ["Green Day" ,"Linkin Park","Gorillaz","Eminem","Fall Out Boy"];
 var random = bandNames[Math.floor(Math.random()*bandNames.length)];
 var answerArray = [];
 
-document.getElementById("#answer") === random;
+//add pictures to each band name
 
+//Set the answer array to _ _ _ _ _ and random word length
 for (var i = 0; i < random.length; i++) {
     answerArray[i] = "_";
    }
    var remainingLetters = random.length;
 
+//Get user imput and indentify what letter it is
 document.onkeyup = function(e){
     var letter = e.key.toLowerCase();
     console.log(letter);
@@ -23,7 +26,15 @@ document.onkeyup = function(e){
         }
     }
 }
+//compare the letter to answers letters
 
+//change guess count and letters guessed
+
+//update word if correct letters
+
+//if run out of guesses show answer and reset
+
+//if correct update wins, show picture, and reset
 
 if(document.getElementById("#word") === random){
     wins++
