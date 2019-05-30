7d8292eb04d45307653bacf43c4c63193a6d3336
Tyler Nichols <tegulator98@gmail.com>
2019-05-29 19:30:59 -0600

    moved divs, got to write to HTML, reset function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,55 @@
 //set var and arrays
 var wins = 0;
 var guessRemains = 12
-var guessLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var bandNames = ["Green Day" ,"Linkin Park","Gorillaz","Eminem","Fall Out Boy"];
+var guessLetters = []
+var bandNames = ["greenday" ,"linkinpark","gorillaz","eminem","falloutboy"];
 var random = bandNames[Math.floor(Math.random()*bandNames.length)];
 var answerArray = [];
+//add pictures to each band name in objects
 
-//add pictures to each band name
 
 //Set the answer array to _ _ _ _ _ and random word length
 for (var i = 0; i < random.length; i++) {
-    answerArray[i] = "_";
+    console.log(random[i]);
+        answerArray[i] = "_";
    }
    var remainingLetters = random.length;
-  alert(answerArray.join(""));
+  
 
 //Get user imput and indentify what letter it is
 document.onkeyup = function(e){
     var letter = e.key.toLowerCase();
+    guessLetters.push(letter);
+    console.log(guessLetters);
     console.log(letter);
     //compare the letter to answers letters
-    for(var i = 0; i < bandNames.length; i++){
+    for(var i = 0; i < random.length; i++){
         if(random[i] === letter){
             answerArray[i] = letter;
             remainingLetters--;
         }
     }
+    //if correct update wins, show picture, and reset
+    if(answerArray == random){
+        wins++
+    }
+    guessRemains--
+    if(guessRemains === -1){
+        alert("You Lost");
+        reset();
+        document.getElementById("word").textContent = "_ _ _ _ _ _ _";
+    }
+    document.getElementById('word').textContent = answerArray;
+    document.getElementById('guesses').textContent = guessRemains;
+    document.getElementById('guessed-letters').textContent = guessLetters;
+    document.getElementById('wins').textContent = wins;
 }
-
+function reset(){
+    guessRemains=12;
+    answerArray= [];
+    guessLetters= [];
+}
+//textContent puts it in the page
 
 
 //change guess count and letters guessed
@@ -36,9 +58,8 @@ document.onkeyup = function(e){
 
 //if run out of guesses show answer and reset
 
-//if correct update wins, show picture, and reset
 
-if(document.getElementById("#word") === random){
-    wins++
-}
-console.log(random);
+
+
+
+console.log(wins);
