79581311086bd67208570753a7aaa4967a81e2c1
Tyler Nichols <tegulator98@gmail.com>
2019-05-29 23:05:27 -0600

    Cleaned up code. pseudo coded the win and picture.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,12 @@
 var wins = 0;
 var guessRemains = 12
 var guessLetters = []
-var bandNames = ["greenday" ,"linkinpark","gorillaz","eminem","falloutboy"];
+//add pictures to each band name in objects
+var bandNames = ["greenday", "linkinpark", "gorillaz", "eminem", "falloutboy"];
 var random = bandNames[Math.floor(Math.random()*bandNames.length)];
 var answerArray = [];
-//add pictures to each band name in objects
+var answerCheck = answerArray;
+
 
 
 //Set the answer array to _ _ _ _ _ and random word length
@@ -22,44 +24,46 @@ document.onkeyup = function(e){
     guessLetters.push(letter);
     console.log(guessLetters);
     console.log(letter);
+    
     //compare the letter to answers letters
     for(var i = 0; i < random.length; i++){
         if(random[i] === letter){
             answerArray[i] = letter;
             remainingLetters--;
         }
+        
+        
     }
     //if correct update wins, show picture, and reset
-    if(answerArray == random){
-        wins++
+  for(var j = 0; j< random.length;)
+    if(answerCheck===answerArray){
+        wins++;
+        reset();
     }
+        
+    
+    //if run out of guesses reset
     guessRemains--
     if(guessRemains === -1){
         alert("You Lost");
         reset();
         document.getElementById("word").textContent = "_ _ _ _ _ _ _";
     }
+    //update word if correct letters
     document.getElementById('word').textContent = answerArray;
+    //change guess count and letters guessed
     document.getElementById('guesses').textContent = guessRemains;
     document.getElementById('guessed-letters').textContent = guessLetters;
     document.getElementById('wins').textContent = wins;
 }
+
+
+
+
 function reset(){
     guessRemains=12;
     answerArray= [];
     guessLetters= [];
 }
-//textContent puts it in the page
-
-
-//change guess count and letters guessed
-
-//update word if correct letters
-
-//if run out of guesses show answer and reset
-
-
-
-
-
+console.log(random);
 console.log(wins);
